6c365154355f25870c01801f2bda080ae950d246
Jeewon Kim <108987773+jeewonkimm2@users.noreply.github.com>
2023-03-15 22:30:15 +0900

    greedy

---
+++ coding_test/ch3_greedy/Q1.py
@@ -0,0 +1,16 @@
+# p311
+
+n = int(input())
+data = list(map(int, input().split()))
+
+result = 0
+count = 0
+
+data.sort()
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+        
+print(result)
